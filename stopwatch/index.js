940fa4e42147cd683f927fc28457aa480f2418bb
abhinaba1999 <subhamsana700@gmail.com>
2023-09-14 01:27:00 +0530

    stop watch

---
+++ stopwatch/index.js
@@ -0,0 +1,49 @@
+let seconds=0;
+let minutes=0;
+let hours=0;
+let displaytime=document.getElementById("display")
+let timer=null;
+
+
+function stopwatch(){
+    seconds++;
+    if(seconds==60){
+        seconds=0;
+        minutes++;
+        if(minutes==60){
+            minutes=0;
+            hours++;
+        }
+    }
+    let h= hours<10 ? "0"+hours:hours;
+    let m= minutes<10 ? "0"+minutes:minutes;
+    let s= seconds<10 ? "0"+seconds:seconds;
+    
+    displaytime.innerHTML=`${h}:${m}:${s}`
+    displaytime.innerText=`${h}:${m}:${s}`
+}
+
+
+function watchStart(){
+    if(timer!=null){
+        clearInterval(timer);
+    }
+    timer=setInterval(stopwatch,1000);
+}
+function pause(){
+  clearInterval(timer);
+}
+function reset(){
+    
+        clearInterval(timer)
+        seconds=0;
+        hours=0;
+        minutes=0;
+        displaytime.innerText="00:00:00";
+       displaytime.innerHTML="00:00:00";
+       timer=null;
+    
+   
+   
+}
+
