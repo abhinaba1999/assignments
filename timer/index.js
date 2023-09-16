75e74d8a7e5f65e70992035dc6f6aeaee68766e8
abhinaba1999 <subhamsana700@gmail.com>
2023-09-16 14:36:30 +0530

    timer code

--- timer/index.js
+++ timer/index.js
@@ -1,41 +1,77 @@
 
-let sec=document.getElementById("sacend")
-let minit=document.getElementById("minit")
-let timer=null;
-let displaytime=document.getElementById("display");
-let secend=null;
-let minits=null;
+ let h = document.getElementById('hour')
+let m = document.getElementById('min')
+let s = document.getElementById('sec')
+let start = document.getElementById('start')
+let reset = document.getElementById('reset')
+let sec =0;
+let min=0;
+let hour=0;
+var countdownInterval;
+var targetTime;
 
-  if(sec.value != undefined){
-   secend=sec.value;
-   if(minit.value!=undefined){
-    minits=minit.value;
-   }
-  }
-    let new_minit=minits*60;
 
-function sstart(){
-    timer=setInterval(starttimer,1000);
-    displaytime.innerText=minit+":"+secend;
+let runFlag = true;
 
+
+start.addEventListener('click', function(){
+if(runFlag){
+    runFlag=false;
     
-}
-function pause(){
+    hour = +h.value;
+    min = +m.value;
+    sec = +s.value
+    targetTime = hour * 3600 + min * 60 + sec;
+    countdownInterval= setInterval(runner,100)
+console.log(hour,min,sec,targetTime)
+}else{
+
+    runFlag=true
+    start.innerText='Start Countdown'
+    clearInterval(countdownInterval);
 
 }
-function reset(){
+})
 
-}
-function starttimer(){
-    
-    secend--;
-    if(secend==0){
-     secend=0;
-        minits--;
-        if(minits==0){
-            minits=0;
-            clearInterval(timer);
+function runner(){
+
+if (targetTime <= 0) {
+            clearInterval(countdownInterval);
+            document.getElementById("timer").textContent = "00:00:00";
+            document.getElementById('hour').value = 0;
+            document.getElementById('min').value = 0;
+            document.getElementById('sec').value = 0;
+        } else {
+            const hour = Math.floor(targetTime / 3600);
+            const minute = Math.floor((targetTime % 3600) / 60);
+            const second = targetTime % 60;
+            document.getElementById("timer").textContent = `${pad(hour)}:${pad(minute)}:${pad(second)}`;
+            
+            start.innerText='Pause Countdown'
+            targetTime--;
         }
+}
+
+reset.addEventListener('click',function(){
+runFlag=true
+start.innerText='Start Countdown'
+
+clearInterval(countdownInterval);
+document.getElementById("timer").textContent = "00:00:00";
+document.getElementById('hour').value = ''
+document.getElementById('min').value= ""
+document.getElementById('sec').value = ""
+
+
+
+})
+
+
+function pad(number) {
+        if(number<10){
+            return `0${number}`
+        }
+        return number
     }
 
-}
+
